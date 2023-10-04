5b9c42890d887d1e34a30ae9b28abb92ad74ff1c
Lucas Vinícius <j.lucasvinicius03@gmail.com>
2023-10-04 10:49:09 -0300

    Nav links

--- bs-config.js
+++ bs-config.js
@@ -2,9 +2,5 @@ module.exports = {
   injectChanges: false,
   files: ["./**/*.{html,htm,css,js}"],
   watchOptions: { ignored: "node_modules" },
-  server: true,
-
-  // Looks kind of ugly in the url but keeps the files organized.
-  // Tried using the index option but it messes up the relative paths.
-  startPath: "public/index.html",
+  server: "public",
 };
